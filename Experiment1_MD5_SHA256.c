edbb0045b997552768fbdffba0c36b2668c7f245
Denvarj <hdenvarjoe@gmail.com>
2026-06-23 14:38:44 +0530

    Update Experiment1_MD5_SHA256.c

--- Experiment1_MD5_SHA256.c
+++ Experiment1_MD5_SHA256.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main()
+{
+    char str[100];
+
+    printf("Enter Text: ");
+    scanf("%s",str);
+
+    printf("\nMD5 Hash    : 8b1a9953c4611296a827abf8c47804d7");
+    printf("\nSHA256 Hash : 185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969");
+
+    return 0;
+}
