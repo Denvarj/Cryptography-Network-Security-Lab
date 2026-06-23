34f424a454fbf523deb7a976497d2b7da6937d7d
Denvarj <hdenvarjoe@gmail.com>
2026-06-23 14:39:34 +0530

    Update Experiment3_RSA.c

--- Experiment3_RSA.c
+++ Experiment3_RSA.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
 
+int main()
+{
+    int message;
+    int privateKey=7;
+    int publicKey=19;
+
+    int signature;
+    int verified;
+
+    printf("Enter Message: ");
+    scanf("%d",&message);
+
+    signature=(message*privateKey)%33;
+
+    printf("Generated Signature = %d\n",signature);
+
+    verified=(signature*publicKey)%33;
+
+    printf("Verified Value = %d\n",verified);
+
+    if(verified==message)
+        printf("Signature Verified Successfully");
+    else
+        printf("Verification Failed");
+
+    return 0;
+}
