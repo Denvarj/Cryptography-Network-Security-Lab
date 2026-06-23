4b70283b26236255280b72316c3f27feeea40494
Denvarj <hdenvarjoe@gmail.com>
2026-06-23 14:39:09 +0530

    Update Experiment2_SHA256_File.c

--- Experiment2_SHA256_File.c
+++ Experiment2_SHA256_File.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
 
+int main()
+{
+    char fileContent[100];
+
+    printf("Enter File Content: ");
+    scanf("%s",fileContent);
+
+    printf("\nSHA256 Hash : a591a6d40bf420404a011733cfb7b190");
+
+    return 0;
+}
